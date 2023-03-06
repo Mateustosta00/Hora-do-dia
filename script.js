9ebd93b3f118d9b532ec51843959aed42625cb55
Mateus Tosta <102609187+Mateustosta00@users.noreply.github.com>
2023-03-06 15:01:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function carregar() {
+
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = 'Agora são exatamente ' + hora + ' hora.'
+
+    if (hora >= 0 && hora < 12) {
+        //BOMDIA
+        msg.innerHTML += 'Bom Dia!'
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#6782f0'
+
+
+    } else if (hora >= 12 && hora < 18) {
+        msg.innerHTML += 'Boa Tarde!'
+        img.src = 'fototarde.png'
+        document.body.style.background = '#95904b'
+
+    } else {
+        //boa noite
+        msg.innerHTML += 'Boa Noite!'
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#292a3f'
+    }
+
+
+}

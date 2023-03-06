57abd3ca1e0bedd39e40129fb3b078d7a4c37330
Mateus Tosta <102609187+Mateustosta00@users.noreply.github.com>
2023-03-06 15:54:58 -0300

    correção de frase

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
 
-    msg.innerHTML = 'Agora são exatamente ' + hora + ' hora.'
+    msg.innerHTML = 'Agora são exatamente ' + hora + ' horas.'
 
     if (hora >= 0 && hora < 12) {
         //BOMDIA
